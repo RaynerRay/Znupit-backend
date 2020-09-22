3d3e7a78fea1a153c53a65f959fb5ab68b386580
rayner <f13ray@gmail.com>
2020-09-22 11:28:35 +0200

    added user registration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("./verifyToken");
 
 const Company = require("./models/Company"); //including the company model
 const Category = require("./models/Category");
+const User = require("./models/User");
 
 router.get("/companies", async (req, res) => {
   try {
@@ -15,7 +19,7 @@ router.get("/companies", async (req, res) => {
 });
 
 // create a company
-router.post("/companies", async (req, res) => {
+router.post("/companies", auth, async (req, res) => {
   try {
     const { name } = req.body;
     const { summary } = req.body;
@@ -137,4 +141,42 @@ router.get("/companies/category/:id", async (req, res) => {
   }
 });
 
+//register a user
+router.post("/register", async (req, res) => {
+  //check if the user is already in database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exist");
+
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  //create a new User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  //check if the email exist
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email or Password is wrong");
+
+  //is password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  //create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
+});
 module.exports = router;
